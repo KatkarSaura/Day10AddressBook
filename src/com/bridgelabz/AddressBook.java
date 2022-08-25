86bc50bc0949379b014bbf7231c940bac82a6065
Pooja <pooja.katkr@gmail.com>
2022-08-25 23:39:50 +0530

    Added new contacts in AddressBook

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -9,28 +9,26 @@ import java.util.Scanner;
 	    Scanner s = new Scanner(System.in);
 	    ArrayList<Person> list = new ArrayList<Person>();
 	    
-	    public void operation(ArrayList<Person> arrayRead) {
-	        list = arrayRead;
-	        boolean status = true;
-	        do {
-	            System.out.println("Choose Operation you want to do");
-	            System.out.println("1. Add\t2. Edit\t3. delete\t4. Back");
-	            switch (s.nextInt()) {
-	                case 1:
-	                    add();
-	                    break;
-	                case 2:
-	                    edit();
-	                    break;
-	                case 3:
-	                    delete();
-	                    break;
-	                case 4:
-	                    status = false;
-	                    break;
-	            }
-
-	
-
-
+   public void operation(ArrayList<Person> arrayRead) {
+	    list = arrayRead;
+	    boolean status = true;
+	    do {
+	      System.out.println("Choose Operation you want to do");
+	      System.out.println("1. Add\t2. Edit\t3. delete\t4. Back");
+	    switch (s.nextInt()) {
+	      case 1:
+	           add();
+	           break; 
+	      case 2:
+	           edit();
+               break;
+	      case 3:
+	           delete();
+	           break;
+	      case 4:
+	           status = false;
+	           break;
+          }
+	    
+	    	    
 }
