fac5592bbbbb3fc2c988632efa909db21acb1d48
Pooja <pooja.katkr@gmail.com>
2022-08-25 22:50:52 +0530

    Create contact in AddressBook

---
+++ src/com/bridgelabz/AddressBook.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Scanner;
+
+	public class AddressBook implements AddressBookInterface {
+	    Scanner s = new Scanner(System.in);
+	    ArrayList<Person> list = new ArrayList<Person>();
+
+	
+
+
+}
