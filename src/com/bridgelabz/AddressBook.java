2fa2a2a4642bb56b2fa54574c2cd53fa174c66ae
Pooja <pooja.katkr@gmail.com>
2022-08-25 23:17:05 +0530

    Add new contact to AddressBook added

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -8,6 +8,27 @@ import java.util.Scanner;
 	public class AddressBook implements AddressBookInterface {
 	    Scanner s = new Scanner(System.in);
 	    ArrayList<Person> list = new ArrayList<Person>();
+	    
+	    public void operation(ArrayList<Person> arrayRead) {
+	        list = arrayRead;
+	        boolean status = true;
+	        do {
+	            System.out.println("Choose Operation you want to do");
+	            System.out.println("1. Add\t2. Edit\t3. delete\t4. Back");
+	            switch (s.nextInt()) {
+	                case 1:
+	                    add();
+	                    break;
+	                case 2:
+	                    edit();
+	                    break;
+	                case 3:
+	                    delete();
+	                    break;
+	                case 4:
+	                    status = false;
+	                    break;
+	            }
 
 	
 
